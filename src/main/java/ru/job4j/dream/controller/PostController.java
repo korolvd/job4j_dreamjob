70ef86e3a5ddcecad80e4c678fe47701fe44bf71
korolvd <ffirin@mail.ru>
2022-03-17 10:56:07 +0500

    3. PostController.savePost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dream/controller/PostController.java
+++ src/main/java/ru/job4j/dream/controller/PostController.java
@@ -3,6 +3,8 @@ package ru.job4j.dream.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.PostStore;
 
@@ -19,7 +21,12 @@ public class PostController {
 
     @GetMapping("/addPost")
     public String addPost(Model model) {
-        model.addAttribute("post", new Post(0, "Заполните поле"));
         return "addPost";
+    }
+
+    @PostMapping("/savePost")
+    public String savePost(@ModelAttribute Post post) {
+        store.add(post);
+        return "redirect:/posts";
     }
 }
