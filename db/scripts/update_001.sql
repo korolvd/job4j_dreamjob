cb652667d474c8df556412b1d5848ea42001585a
korolvd <ffirin@mail.ru>
2022-03-30 09:53:37 +0500

    4. Многопоточность в базе данных [#504860 #282006]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,26 +1,37 @@
 CREATE TABLE IF NOT EXISTS city
 (
-    id SERIAL PRIMARY KEY,
+    id   SERIAL PRIMARY KEY,
     name TEXT
 );
 
-INSERT INTO city(name) VALUES ('Москва');
-INSERT INTO city(name) VALUES ('СПб');
-INSERT INTO city(name) VALUES ('Екб');
+INSERT INTO city(name)
+VALUES ('Москва');
+INSERT INTO city(name)
+VALUES ('СПб');
+INSERT INTO city(name)
+VALUES ('Екб');
 
 CREATE TABLE IF NOT EXISTS post
 (
     id      SERIAL PRIMARY KEY,
     name    TEXT,
     visible BOOLEAN,
-    city_id INT REFERENCES city(id)
+    city_id INT REFERENCES city (id)
 );
 
 CREATE TABLE IF NOT EXISTS candidate
 (
-    id   SERIAL PRIMARY KEY,
-    name TEXT,
+    id          SERIAL PRIMARY KEY,
+    name        TEXT,
     description TEXT,
-    created TIMESTAMP,
-    photo bytea
+    created     TIMESTAMP,
+    photo       bytea
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id    SERIAL PRIMARY KEY,
+    name  TEXT,
+    email TEXT,
+    CONSTRAINT email_unique UNIQUE (email)
 );
